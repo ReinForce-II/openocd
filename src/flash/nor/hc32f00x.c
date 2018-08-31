a019cb9fec9e34bb48b1a28567b4f40f35949aa8
Reinforce-II <fate@eastal.com>
2018-09-01 00:54:10 +0800

    format code

--- src/flash/nor/hc32f00x.c
+++ src/flash/nor/hc32f00x.c
@@ -60,257 +60,237 @@
 
 static int hc32_mass_erase(struct flash_bank *bank);
 
-static uint32_t hc32_get_flash_reg(struct flash_bank *bank, uint32_t reg)
-{
-    uint32_t value;
-    struct target *target = bank->target;
-    target_read_u32(target, FLASH_REG_BASE_B0 + reg, &value);
-    return value;
+static uint32_t hc32_get_flash_reg(struct flash_bank *bank, uint32_t reg) {
+  uint32_t value;
+  struct target *target = bank->target;
+  target_read_u32(target, FLASH_REG_BASE_B0 + reg, &value);
+  return value;
 }
 
-static void hc32_set_flash_reg(struct flash_bank *bank, uint32_t reg, uint32_t dat)
-{
-    struct target *target = bank->target;
-    target_write_u32(target, FLASH_REG_BASE_B0 + HC32_FLASH_BYPASS, 0x5a5a);
-    target_write_u32(target, FLASH_REG_BASE_B0 + HC32_FLASH_BYPASS, 0xa5a5);
-    target_write_u32(target, FLASH_REG_BASE_B0 + reg, dat);
+static void hc32_set_flash_reg(struct flash_bank *bank, uint32_t reg,
+                               uint32_t dat) {
+  struct target *target = bank->target;
+  target_write_u32(target, FLASH_REG_BASE_B0 + HC32_FLASH_BYPASS, 0x5a5a);
+  target_write_u32(target, FLASH_REG_BASE_B0 + HC32_FLASH_BYPASS, 0xa5a5);
+  target_write_u32(target, FLASH_REG_BASE_B0 + reg, dat);
 }
 
-static int hc32_wait_status_busy(struct flash_bank *bank, int timeout)
-{
-    int retval = ERROR_OK;
+static int hc32_wait_status_busy(struct flash_bank *bank, int timeout) {
+  int retval = ERROR_OK;
 
-    /* wait for busy to clear */
-    for (;;)
-    {
-        if ((hc32_get_flash_reg(bank, HC32_FLASH_CR) & FLASH_CR_BUSY) == 0)
-            break;
-        if (timeout-- <= 0)
-        {
-            LOG_ERROR("timed out waiting for flash");
-            return ERROR_FAIL;
-        }
-        alive_sleep(1);
+  /* wait for busy to clear */
+  for (;;) {
+    if ((hc32_get_flash_reg(bank, HC32_FLASH_CR) & FLASH_CR_BUSY) == 0)
+      break;
+    if (timeout-- <= 0) {
+      LOG_ERROR("timed out waiting for flash");
+      return ERROR_FAIL;
     }
-    return retval;
+    alive_sleep(1);
+  }
+  return retval;
 }
 
-static int hc32_mass_erase(struct flash_bank *bank)
-{
-    int retval = ERROR_OK;
-    struct target *target = bank->target;
-    uint32_t slock = hc32_get_flash_reg(bank, HC32_FLASH_SLOCK);
-    hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, 0xffff);
+static int hc32_mass_erase(struct flash_bank *bank) {
+  int retval = ERROR_OK;
+  struct target *target = bank->target;
+  uint32_t slock = hc32_get_flash_reg(bank, HC32_FLASH_SLOCK);
+  hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, 0xffff);
 
-    uint32_t cr = hc32_get_flash_reg(bank, HC32_FLASH_CR);
+  uint32_t cr = hc32_get_flash_reg(bank, HC32_FLASH_CR);
 
-    hc32_set_flash_reg(bank, HC32_FLASH_CR, cr | 0x3);
-    target_write_u32(target, FLASH_BASE, 0x00000000);
+  hc32_set_flash_reg(bank, HC32_FLASH_CR, cr | 0x3);
+  target_write_u32(target, FLASH_BASE, 0x00000000);
+  retval = hc32_wait_status_busy(bank, FLASH_ERASE_TIMEOUT);
+
+  hc32_set_flash_reg(bank, HC32_FLASH_CR, cr);
+  hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, slock);
+  return retval;
+}
+
+static int hc32_erase(struct flash_bank *bank, int first, int last) {
+  struct target *target = bank->target;
+  int i;
+
+  if (bank->target->state != TARGET_HALTED) {
+    LOG_ERROR("Target not halted");
+    return ERROR_TARGET_NOT_HALTED;
+  }
+  const struct armv7m_common *cm = target_to_armv7m(target);
+  uint32_t R_PC;
+  cm->load_core_reg_u32(target, ARMV7M_PC, &R_PC);
+  cm->store_core_reg_u32(target, ARMV7M_PC, 0x20000000);
+
+  if ((first == 0) && (last == (bank->num_sectors - 1)))
+    return hc32_mass_erase(bank);
+
+  uint32_t slock = hc32_get_flash_reg(bank, HC32_FLASH_SLOCK);
+  hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, 0xffff);
+  uint32_t cr = hc32_get_flash_reg(bank, HC32_FLASH_CR);
+  int retval = ERROR_OK;
+  hc32_set_flash_reg(bank, HC32_FLASH_CR, (cr & 0xfffffffc) | 0x2);
+  for (i = first; i <= last; i++) {
+    target_write_u32(target, FLASH_BASE + i * FLASH_SECTOR_SIZE, 0x00000000);
     retval = hc32_wait_status_busy(bank, FLASH_ERASE_TIMEOUT);
-
-    hc32_set_flash_reg(bank, HC32_FLASH_CR, cr);
-    hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, slock);
-    return retval;
+    if (retval != ERROR_OK) {
+      LOG_ERROR("Erase error at sector %d", i);
+      break;
+    }
+  }
+  hc32_set_flash_reg(bank, HC32_FLASH_CR, cr);
+  hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, slock);
+  return retval;
 }
 
-static int hc32_erase(struct flash_bank *bank, int first, int last)
-{
-    struct target *target = bank->target;
-    int i;
+static int hc32_protect(struct flash_bank *bank, int set, int first, int last) {
+  int i;
 
-    if (bank->target->state != TARGET_HALTED)
-    {
-        LOG_ERROR("Target not halted");
-        return ERROR_TARGET_NOT_HALTED;
+  uint32_t slock = hc32_get_flash_reg(bank, HC32_FLASH_SLOCK);
+  for (i = first; i <= last; i++) {
+    if (set) {
+      slock &= (~(1 << (i / 4)));
+    } else {
+      slock |= (1 << (i / 4));
     }
-    const struct armv7m_common *cm = target_to_armv7m(target);
-    uint32_t R_PC;
-    cm->load_core_reg_u32(target, ARMV7M_PC, &R_PC);
-    cm->store_core_reg_u32(target, ARMV7M_PC, 0x20000000);
-
-    if ((first == 0) && (last == (bank->num_sectors - 1)))
-        return hc32_mass_erase(bank);
-
-    uint32_t slock = hc32_get_flash_reg(bank, HC32_FLASH_SLOCK);
-    hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, 0xffff);
-    uint32_t cr = hc32_get_flash_reg(bank, HC32_FLASH_CR);
-    int retval = ERROR_OK;
-    hc32_set_flash_reg(bank, HC32_FLASH_CR, (cr & 0xfffffffc) | 0x2);
-    for (i = first; i <= last; i++)
-    {
-        target_write_u32(target, FLASH_BASE + i * FLASH_SECTOR_SIZE, 0x00000000);
-        retval = hc32_wait_status_busy(bank, FLASH_ERASE_TIMEOUT);
-        if (retval != ERROR_OK)
-        {
-            LOG_ERROR("Erase error at sector %d", i);
-            break;
-        }
-    }
-    hc32_set_flash_reg(bank, HC32_FLASH_CR, cr);
-    hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, slock);
-    return retval;
-}
-
-static int hc32_protect(struct flash_bank *bank, int set, int first, int last)
-{
-    int i;
-
-    uint32_t slock = hc32_get_flash_reg(bank, HC32_FLASH_SLOCK);
-    for (i = first; i <= last; i++)
-    {
-        if (set)
-        {
-            slock &= (~(1 << (i / 4)));
-        }
-        else
-        {
-            slock |= (1 << (i / 4));
-        }
-    }
-    hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, slock);
-    return ERROR_OK;
+  }
+  hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, slock);
+  return ERROR_OK;
 }
 
 static int hc32_write_block(struct flash_bank *bank, const uint8_t *buffer,
-		uint32_t offset, uint32_t count)
-{
-	struct target *target = bank->target;
-	uint32_t buffer_size = 16384;
-	struct working_area *write_algorithm;
-	struct working_area *source;
-	uint32_t address = bank->base + offset;
-	struct reg_param reg_params[5];
-	struct armv7m_algorithm armv7m_info;
-	int retval = ERROR_OK;
+                            uint32_t offset, uint32_t count) {
+  struct target *target = bank->target;
+  uint32_t buffer_size = 16384;
+  struct working_area *write_algorithm;
+  struct working_area *source;
+  uint32_t address = bank->base + offset;
+  struct reg_param reg_params[5];
+  struct armv7m_algorithm armv7m_info;
+  int retval = ERROR_OK;
 
-	static const uint8_t hc32_flash_write_code[] = {
+  static const uint8_t hc32_flash_write_code[] = {
 #include "../../../contrib/loaders/flash/hc32f00x/hc32f00x.inc"
-	};
+  };
 
-	/* flash write code */
-	if (target_alloc_working_area(target, sizeof(hc32_flash_write_code),
-			&write_algorithm) != ERROR_OK) {
-		LOG_WARNING("no working area available, can't do block memory writes");
-		return ERROR_TARGET_RESOURCE_NOT_AVAILABLE;
-	}
+  /* flash write code */
+  if (target_alloc_working_area(target, sizeof(hc32_flash_write_code),
+                                &write_algorithm) != ERROR_OK) {
+    LOG_WARNING("no working area available, can't do block memory writes");
+    return ERROR_TARGET_RESOURCE_NOT_AVAILABLE;
+  }
 
-	retval = target_write_buffer(target, write_algorithm->address,
-			sizeof(hc32_flash_write_code), hc32_flash_write_code);
-	if (retval != ERROR_OK) {
-		target_free_working_area(target, write_algorithm);
-		return retval;
-	}
+  retval =
+      target_write_buffer(target, write_algorithm->address,
+                          sizeof(hc32_flash_write_code), hc32_flash_write_code);
+  if (retval != ERROR_OK) {
+    target_free_working_area(target, write_algorithm);
+    return retval;
+  }
 
-	/* memory buffer */
-	while (target_alloc_working_area_try(target, buffer_size, &source) != ERROR_OK) {
-		buffer_size /= 2;
-		buffer_size &= ~3UL; /* Make sure it's 4 byte aligned */
-		if (buffer_size <= 256) {
-			/* we already allocated the writing code, but failed to get a
-			 * buffer, free the algorithm */
-			target_free_working_area(target, write_algorithm);
+  /* memory buffer */
+  while (target_alloc_working_area_try(target, buffer_size, &source) !=
+         ERROR_OK) {
+    buffer_size /= 2;
+    buffer_size &= ~3UL; /* Make sure it's 4 byte aligned */
+    if (buffer_size <= 256) {
+      /* we already allocated the writing code, but failed to get a
+       * buffer, free the algorithm */
+      target_free_working_area(target, write_algorithm);
 
-			LOG_WARNING("no large enough working area available, can't do block memory writes");
-			return ERROR_TARGET_RESOURCE_NOT_AVAILABLE;
-		}
-	}
+      LOG_WARNING("no large enough working area available, can't do block "
+                  "memory writes");
+      return ERROR_TARGET_RESOURCE_NOT_AVAILABLE;
+    }
+  }
 
-	init_reg_param(&reg_params[0], "r0", 32, PARAM_IN_OUT);	/* flash base (in), status (out) */
-	init_reg_param(&reg_params[1], "r1", 32, PARAM_OUT);	/* count (halfword-16bit) */
-	init_reg_param(&reg_params[2], "r2", 32, PARAM_OUT);	/* buffer start */
-	init_reg_param(&reg_params[3], "r3", 32, PARAM_OUT);	/* buffer end */
-	init_reg_param(&reg_params[4], "r4", 32, PARAM_IN_OUT);	/* target address */
+  init_reg_param(&reg_params[0], "r0", 32,
+                 PARAM_IN_OUT); /* flash base (in), status (out) */
+  init_reg_param(&reg_params[1], "r1", 32,
+                 PARAM_OUT); /* count (halfword-16bit) */
+  init_reg_param(&reg_params[2], "r2", 32, PARAM_OUT);    /* buffer start */
+  init_reg_param(&reg_params[3], "r3", 32, PARAM_OUT);    /* buffer end */
+  init_reg_param(&reg_params[4], "r4", 32, PARAM_IN_OUT); /* target address */
 
-	buf_set_u32(reg_params[0].value, 0, 32, FLASH_REG_BASE_B0);
-	buf_set_u32(reg_params[1].value, 0, 32, count);
-	buf_set_u32(reg_params[2].value, 0, 32, source->address);
-	buf_set_u32(reg_params[3].value, 0, 32, source->address + source->size);
-	buf_set_u32(reg_params[4].value, 0, 32, address);
+  buf_set_u32(reg_params[0].value, 0, 32, FLASH_REG_BASE_B0);
+  buf_set_u32(reg_params[1].value, 0, 32, count);
+  buf_set_u32(reg_params[2].value, 0, 32, source->address);
+  buf_set_u32(reg_params[3].value, 0, 32, source->address + source->size);
+  buf_set_u32(reg_params[4].value, 0, 32, address);
 
-	armv7m_info.common_magic = ARMV7M_COMMON_MAGIC;
-	armv7m_info.core_mode = ARM_MODE_THREAD;
+  armv7m_info.common_magic = ARMV7M_COMMON_MAGIC;
+  armv7m_info.core_mode = ARM_MODE_THREAD;
 
-	retval = target_run_flash_async_algorithm(target, buffer, count, 1,
-			0, NULL,
-			5, reg_params,
-			source->address, source->size,
-			write_algorithm->address, 0,
-			&armv7m_info);
+  retval = target_run_flash_async_algorithm(
+      target, buffer, count, 1, 0, NULL, 5, reg_params, source->address,
+      source->size, write_algorithm->address, 0, &armv7m_info);
 
-	if (retval == ERROR_FLASH_OPERATION_FAILED) {
-			LOG_ERROR("flash memory not erased before writing");
-	}
+  if (retval == ERROR_FLASH_OPERATION_FAILED) {
+    LOG_ERROR("flash memory not erased before writing");
+  }
 
-	target_free_working_area(target, source);
-	target_free_working_area(target, write_algorithm);
+  target_free_working_area(target, source);
+  target_free_working_area(target, write_algorithm);
 
-	destroy_reg_param(&reg_params[0]);
-	destroy_reg_param(&reg_params[1]);
-	destroy_reg_param(&reg_params[2]);
-	destroy_reg_param(&reg_params[3]);
-	destroy_reg_param(&reg_params[4]);
+  destroy_reg_param(&reg_params[0]);
+  destroy_reg_param(&reg_params[1]);
+  destroy_reg_param(&reg_params[2]);
+  destroy_reg_param(&reg_params[3]);
+  destroy_reg_param(&reg_params[4]);
 
-	return retval;
+  return retval;
 }
 
 static int hc32_write(struct flash_bank *bank, const uint8_t *buffer,
-                      uint32_t offset, uint32_t count)
-{
-    struct target *target = bank->target;
-    uint32_t i;
+                      uint32_t offset, uint32_t count) {
+  struct target *target = bank->target;
+  uint32_t i;
 
-    if (bank->target->state != TARGET_HALTED)
-    {
-        LOG_ERROR("Target not halted");
-        return ERROR_TARGET_NOT_HALTED;
-    }
-    const struct armv7m_common *cm = target_to_armv7m(target);
-    uint32_t R_PC;
-    cm->load_core_reg_u32(target, ARMV7M_PC, &R_PC);
-    cm->store_core_reg_u32(target, ARMV7M_PC, 0x20000000);
+  if (bank->target->state != TARGET_HALTED) {
+    LOG_ERROR("Target not halted");
+    return ERROR_TARGET_NOT_HALTED;
+  }
+  const struct armv7m_common *cm = target_to_armv7m(target);
+  uint32_t R_PC;
+  cm->load_core_reg_u32(target, ARMV7M_PC, &R_PC);
+  cm->store_core_reg_u32(target, ARMV7M_PC, 0x20000000);
 
-    uint32_t slock = hc32_get_flash_reg(bank, HC32_FLASH_SLOCK);
-    hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, 0xffff);
-    uint32_t cr = hc32_get_flash_reg(bank, HC32_FLASH_CR);
-    int retval = ERROR_OK;
-    hc32_set_flash_reg(bank, HC32_FLASH_CR, (cr & 0xfffffffc) | 0x1);
-    uint32_t block_size = 1024;
-    for (i=0; i < count; i += block_size) {
-        if (i > count - block_size) {
-            block_size = count - i;
-        }
-        retval = hc32_write_block(bank, buffer + i, offset + i, block_size);
-        if (retval != ERROR_OK) {
-            break;
-        }
+  uint32_t slock = hc32_get_flash_reg(bank, HC32_FLASH_SLOCK);
+  hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, 0xffff);
+  uint32_t cr = hc32_get_flash_reg(bank, HC32_FLASH_CR);
+  int retval = ERROR_OK;
+  hc32_set_flash_reg(bank, HC32_FLASH_CR, (cr & 0xfffffffc) | 0x1);
+  uint32_t block_size = 1024;
+  for (i = 0; i < count; i += block_size) {
+    if (i > count - block_size) {
+      block_size = count - i;
     }
-    hc32_set_flash_reg(bank, HC32_FLASH_CR, cr);
-    hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, slock);
-    return retval;
+    retval = hc32_write_block(bank, buffer + i, offset + i, block_size);
+    if (retval != ERROR_OK) {
+      break;
+    }
+  }
+  hc32_set_flash_reg(bank, HC32_FLASH_CR, cr);
+  hc32_set_flash_reg(bank, HC32_FLASH_SLOCK, slock);
+  return retval;
 }
 
-static int hc32_probe(struct flash_bank *bank)
-{
-    int i;
-    bank->base = FLASH_BASE;
-    bank->size = FLASH_NUM_SECTORS * FLASH_SECTOR_SIZE;
-    bank->num_sectors = FLASH_NUM_SECTORS;
-    bank->sectors = malloc(sizeof(struct flash_sector) * FLASH_NUM_SECTORS);
+static int hc32_probe(struct flash_bank *bank) {
+  int i;
+  bank->base = FLASH_BASE;
+  bank->size = FLASH_NUM_SECTORS * FLASH_SECTOR_SIZE;
+  bank->num_sectors = FLASH_NUM_SECTORS;
+  bank->sectors = malloc(sizeof(struct flash_sector) * FLASH_NUM_SECTORS);
 
-    for (i = 0; i < FLASH_NUM_SECTORS; i++)
-    {
-        bank->sectors[i].offset = i * FLASH_SECTOR_SIZE;
-        bank->sectors[i].size = FLASH_SECTOR_SIZE;
-        bank->sectors[i].is_erased = -1;
-        bank->sectors[i].is_protected = 1;
-    }
-    return ERROR_OK;
+  for (i = 0; i < FLASH_NUM_SECTORS; i++) {
+    bank->sectors[i].offset = i * FLASH_SECTOR_SIZE;
+    bank->sectors[i].size = FLASH_SECTOR_SIZE;
+    bank->sectors[i].is_erased = -1;
+    bank->sectors[i].is_protected = 1;
+  }
+  return ERROR_OK;
 }
 
-static int hc32_auto_probe(struct flash_bank *bank)
-{
-    return hc32_probe(bank);
-}
+static int hc32_auto_probe(struct flash_bank *bank) { return hc32_probe(bank); }
 
 #if 0
 COMMAND_HANDLER(hc32_handle_part_id_command)
@@ -319,43 +299,36 @@ COMMAND_HANDLER(hc32_handle_part_id_command)
 }
 #endif
 
-static int hc32_protect_check(struct flash_bank *bank)
-{
-    int i;
+static int hc32_protect_check(struct flash_bank *bank) {
+  int i;
 
-    uint32_t slock = hc32_get_flash_reg(bank, HC32_FLASH_SLOCK);
-    for (i = 0; i < bank->num_sectors; i += 4)
-    {
-        if ((slock & (1 << (i / 4))) == 0)
-        {
-            bank->sectors[i].is_protected = 1;
-            bank->sectors[i + 1].is_protected = 1;
-            bank->sectors[i + 2].is_protected = 1;
-            bank->sectors[i + 3].is_protected = 1;
-        }
-        else
-        {
-            bank->sectors[i].is_protected = 0;
-            bank->sectors[i + 1].is_protected = 0;
-            bank->sectors[i + 2].is_protected = 0;
-            bank->sectors[i + 3].is_protected = 0;
-        }
+  uint32_t slock = hc32_get_flash_reg(bank, HC32_FLASH_SLOCK);
+  for (i = 0; i < bank->num_sectors; i += 4) {
+    if ((slock & (1 << (i / 4))) == 0) {
+      bank->sectors[i].is_protected = 1;
+      bank->sectors[i + 1].is_protected = 1;
+      bank->sectors[i + 2].is_protected = 1;
+      bank->sectors[i + 3].is_protected = 1;
+    } else {
+      bank->sectors[i].is_protected = 0;
+      bank->sectors[i + 1].is_protected = 0;
+      bank->sectors[i + 2].is_protected = 0;
+      bank->sectors[i + 3].is_protected = 0;
     }
-    return ERROR_OK;
+  }
+  return ERROR_OK;
 }
 
-static int get_hc32_info(struct flash_bank *bank, char *buf, int buf_size)
-{
-    snprintf(buf, buf_size, "Empty");
-    return ERROR_OK;
+static int get_hc32_info(struct flash_bank *bank, char *buf, int buf_size) {
+  snprintf(buf, buf_size, "Empty");
+  return ERROR_OK;
 }
 
-FLASH_BANK_COMMAND_HANDLER(hc32_flash_bank_command)
-{
-    if (CMD_ARGC < 6)
-        return ERROR_COMMAND_SYNTAX_ERROR;
-    bank->driver_priv = NULL;
-    return ERROR_OK;
+FLASH_BANK_COMMAND_HANDLER(hc32_flash_bank_command) {
+  if (CMD_ARGC < 6)
+    return ERROR_COMMAND_SYNTAX_ERROR;
+  bank->driver_priv = NULL;
+  return ERROR_OK;
 }
 
 struct flash_driver hc32_flash = {

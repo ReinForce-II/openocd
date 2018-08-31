b8f0e392b08733f49a6274a3cabca98677e48b85
Reinforce-II <fate@eastal.com>
2018-09-01 00:26:39 +0800

    use remote op

--- src/flash/nor/hc32f00x.c
+++ src/flash/nor/hc32f00x.c
@@ -171,6 +171,88 @@ static int hc32_protect(struct flash_bank *bank, int set, int first, int last)
     return ERROR_OK;
 }
 
+static int hc32_write_block(struct flash_bank *bank, const uint8_t *buffer,
+		uint32_t offset, uint32_t count)
+{
+	struct target *target = bank->target;
+	uint32_t buffer_size = 16384;
+	struct working_area *write_algorithm;
+	struct working_area *source;
+	uint32_t address = bank->base + offset;
+	struct reg_param reg_params[5];
+	struct armv7m_algorithm armv7m_info;
+	int retval = ERROR_OK;
+
+	static const uint8_t hc32_flash_write_code[] = {
+#include "../../../contrib/loaders/flash/hc32f00x/hc32f00x.inc"
+	};
+
+	/* flash write code */
+	if (target_alloc_working_area(target, sizeof(hc32_flash_write_code),
+			&write_algorithm) != ERROR_OK) {
+		LOG_WARNING("no working area available, can't do block memory writes");
+		return ERROR_TARGET_RESOURCE_NOT_AVAILABLE;
+	}
+
+	retval = target_write_buffer(target, write_algorithm->address,
+			sizeof(hc32_flash_write_code), hc32_flash_write_code);
+	if (retval != ERROR_OK) {
+		target_free_working_area(target, write_algorithm);
+		return retval;
+	}
+
+	/* memory buffer */
+	while (target_alloc_working_area_try(target, buffer_size, &source) != ERROR_OK) {
+		buffer_size /= 2;
+		buffer_size &= ~3UL; /* Make sure it's 4 byte aligned */
+		if (buffer_size <= 256) {
+			/* we already allocated the writing code, but failed to get a
+			 * buffer, free the algorithm */
+			target_free_working_area(target, write_algorithm);
+
+			LOG_WARNING("no large enough working area available, can't do block memory writes");
+			return ERROR_TARGET_RESOURCE_NOT_AVAILABLE;
+		}
+	}
+
+	init_reg_param(&reg_params[0], "r0", 32, PARAM_IN_OUT);	/* flash base (in), status (out) */
+	init_reg_param(&reg_params[1], "r1", 32, PARAM_OUT);	/* count (halfword-16bit) */
+	init_reg_param(&reg_params[2], "r2", 32, PARAM_OUT);	/* buffer start */
+	init_reg_param(&reg_params[3], "r3", 32, PARAM_OUT);	/* buffer end */
+	init_reg_param(&reg_params[4], "r4", 32, PARAM_IN_OUT);	/* target address */
+
+	buf_set_u32(reg_params[0].value, 0, 32, FLASH_REG_BASE_B0);
+	buf_set_u32(reg_params[1].value, 0, 32, count);
+	buf_set_u32(reg_params[2].value, 0, 32, source->address);
+	buf_set_u32(reg_params[3].value, 0, 32, source->address + source->size);
+	buf_set_u32(reg_params[4].value, 0, 32, address);
+
+	armv7m_info.common_magic = ARMV7M_COMMON_MAGIC;
+	armv7m_info.core_mode = ARM_MODE_THREAD;
+
+	retval = target_run_flash_async_algorithm(target, buffer, count, 1,
+			0, NULL,
+			5, reg_params,
+			source->address, source->size,
+			write_algorithm->address, 0,
+			&armv7m_info);
+
+	if (retval == ERROR_FLASH_OPERATION_FAILED) {
+			LOG_ERROR("flash memory not erased before writing");
+	}
+
+	target_free_working_area(target, source);
+	target_free_working_area(target, write_algorithm);
+
+	destroy_reg_param(&reg_params[0]);
+	destroy_reg_param(&reg_params[1]);
+	destroy_reg_param(&reg_params[2]);
+	destroy_reg_param(&reg_params[3]);
+	destroy_reg_param(&reg_params[4]);
+
+	return retval;
+}
+
 static int hc32_write(struct flash_bank *bank, const uint8_t *buffer,
                       uint32_t offset, uint32_t count)
 {
@@ -192,13 +274,10 @@ static int hc32_write(struct flash_bank *bank, const uint8_t *buffer,
     uint32_t cr = hc32_get_flash_reg(bank, HC32_FLASH_CR);
     int retval = ERROR_OK;
     hc32_set_flash_reg(bank, HC32_FLASH_CR, (cr & 0xfffffffc) | 0x1);
-    for (i = 0; i < count; i += 4)
-    {
-        target_write_u32(target, FLASH_BASE + offset + i, *(uint32_t *)(buffer + i));
-        retval = hc32_wait_status_busy(bank, FLASH_ERASE_TIMEOUT);
-        if (retval != ERROR_OK)
-        {
-            LOG_ERROR("Write error at byte %d", i);
+    uint32_t block_size = 64;
+    for (i=0; i < count; i += 16) {
+        retval = hc32_write_block(bank, buffer + i, offset + i, 64);
+        if (retval != ERROR_OK) {
             break;
         }
     }
